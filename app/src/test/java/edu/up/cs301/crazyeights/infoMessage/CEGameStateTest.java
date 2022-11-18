519d531244e9d9084f5e4bb3300805091dac4d04
doemily <111350013+doemily@users.noreply.github.com>
2022-11-17 17:11:26 -0800

    CEGameStateTest

---
+++ app/src/test/java/edu/up/cs301/crazyeights/infoMessage/CEGameStateTest.java
@@ -0,0 +1,44 @@
+package edu.up.cs301.crazyeights.infoMessage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CEGameStateTest {
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void setDrawPile() {
+    }
+
+    @Test
+    public void dealCards() {
+    }
+
+    @Test
+    public void placeCard() {
+    }
+
+    @Test
+    public void getRandomCard() {
+    }
+
+    @Test
+    public void checkCardEligibility() {
+    }
+
+    @Test
+    public void drawCard() {
+    }
+
+    @Test
+    public void setNumPlayerTurn() {
+    }
+
+    @Test
+    public void getPlayerToMove() {
+    }
+}
