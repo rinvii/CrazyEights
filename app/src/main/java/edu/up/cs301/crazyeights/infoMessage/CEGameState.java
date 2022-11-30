ab8ca87c02dc7bbb7a3035b1c96b52e7d84e7f8d
melemai25 <melemai25@up.edu>
2022-11-29 20:51:33 -0800

    Game Logic Alpha (with 8's Alpha) CC turns Game restarts when player runs out of cards scores are tracked (need UI) Game has win conditions and restarts

--- app/src/main/java/edu/up/cs301/crazyeights/infoMessage/CEGameState.java
+++ app/src/main/java/edu/up/cs301/crazyeights/infoMessage/CEGameState.java
@@ -121,7 +121,6 @@ public class CEGameState extends GameState implements Serializable {
             }
             count++;
             Log.e("tallyScores: ", count +":"+ player.getScore());
-            player.setCardsInHand();
             }
         }
 
